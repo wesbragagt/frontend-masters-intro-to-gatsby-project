6b85d6d199eef59ec220f7a695b35771f9757260
weslley.delimabragas <weslley.delimabragasilva@asurion.com>
2021-09-28 11:38:45 -0500

    created two pages

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+// This is Home page for the side
+import * as React from 'react';
+
+// Pages in gatsby has to be default export
+export default function IndexPage() {
+  return <main>Home</main>;
+}
